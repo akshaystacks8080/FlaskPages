cda40664db42820908ad707016e63736056ed3df
akshay <akshay.pednekar@gmail.com>
2024-09-25 02:05:57 -0500

    Implement Flask login manager and configure redirect for unauthenticated users

--- FlaskPages/website/auth.py
+++ FlaskPages/website/auth.py
@@ -2,18 +2,34 @@ from flask import Blueprint, render_template, request, flash, redirect, url_for
 from .models import User
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
+from flask_login import login_user, login_required, logout_user, current_user
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login', methods=['GET','POST'])
 def login():
-    data = request.form
-    print(data)
+    if request.method =='POST':
+        email = request.form.get('email')
+        password = request.form.get('password1')
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash('Logged in successfully!', category='success')
+                login_user(user, remember=True)
+                return redirect(url_for('views.home'))
+            else:
+                flash('Incorrect password, Try again.', category='error')
+        else:
+            flash('Email does not exist.', category='error')
+    
     return render_template("login.html", text="Testing", boolean=True)
 
 @auth.route('/logout')
+@login_required
 def logout():
-    return render_template("logout.html")
+    logout_user()
+    return redirect(url_for('auth.login'))
 
 @auth.route('/sign-up', methods=['GET','POST'])
 def sign_up():
@@ -23,7 +39,10 @@ def sign_up():
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
 
-        if len(email) < 4:
+        user = User.query.filter_by(email=email).first()
+        if user:
+            flash('Email already exist', category='error')
+        elif len(email) < 4:
             flash('Email must be greater than 3 characters.', category='error')
         elif len(first_name)<2:
             flash('First Name must be greater than 1 characters.', category='error')
@@ -33,7 +52,9 @@ def sign_up():
             flash('Password must be at least 7 characters', category='error')
         else:
             new_user = User(email=email, first_name=first_name, password=generate_password_hash(password1))
+            db.session.add(new_user)
             db.session.commit()
+            login_user(user, remember=True)
             flash('Account Created', category='success')
             return redirect(url_for('views.home'))
 
